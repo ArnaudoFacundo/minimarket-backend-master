4e9becc8498806ed5a9d200b7f558abe3d4e4ec5
Facundo Gabriel <89277329+ArnaudoFacundo@users.noreply.github.com>
2023-03-30 22:42:49 -0300

    Add files via upload

---
+++ minimarket-backend-master/routers/rol-router.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { agregarRol } from "../middlewares/rol-middleware.js";
+
+export const rolRouter = express.Router();
+
+// POST / agregar rol a usuario
+rolRouter.post("/", agregarRol);
